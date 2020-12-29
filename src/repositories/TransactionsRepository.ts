a4fe7d107a85065929a7b1f90e6be2a6d80d6941
Fagner JB <fagnerjb@gmail.com>
2020-12-29 18:25:11 -0300

    Solved

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,73 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+   income: number;
+   outcome: number;
+   total: number;
+}
+
+interface TransactionInterface {
+   title: string;
+   value: number;
+   type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+   private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+   constructor() {
+      this.transactions = [];
+   }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+   public all(): Transaction[] {
+      return this.transactions
+   }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+   public getBalance(): Balance {
 
-  public create(): Transaction {
-    // TODO
-  }
+      const transactions = this.all()
+
+      const balance = transactions.reduce((accumulate, item) => {
+
+         accumulate[item.type] += item.value
+
+         if ("income" === item.type) {
+            accumulate.total += item.value
+         }
+         else if ("outcome" === item.type) {
+            accumulate.total -= item.value
+         }
+
+         return accumulate
+
+      }, {
+         income: 0,
+         outcome: 0,
+         total: 0
+      })
+
+      return balance
+   }
+
+   public create({ title, value, type }: TransactionInterface): Transaction {
+
+      if ("outcome" === type) {
+         const balance = this.getBalance()
+
+         if (value > balance.total)
+            throw Error("Don't have enough for this")
+      }
+
+      const transition = new Transaction({
+         title,
+         value,
+         type
+      })
+
+      this.transactions.push(transition)
+
+      return transition
+   }
 }
 
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,28 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionInterface {
+   title: string
+   value: number
+   type: "income" | "outcome"
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+   private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+   constructor(transactionsRepository: TransactionsRepository) {
+      this.transactionsRepository = transactionsRepository;
+   }
 
-  public execute(): Transaction {
-    // TODO
-  }
+   public execute({ title, value, type }: TransactionInterface): Transaction {
+
+      return this.transactionsRepository.create({
+         title,
+         value,
+         type
+      })
+
+   }
 }
 
 export default CreateTransactionService;
